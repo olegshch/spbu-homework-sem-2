68729f684e0383f5b6ffe436cd207c199b1623f4
olegshch <43221802+olegshch@users.noreply.github.com>
2019-04-26 15:18:39 +0300

    Add files via upload

---
+++ Button/Button/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Button
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            button1.MouseEnter += new EventHandler(button1_MouseEnter);
+        }
+        private void button1_MouseEnter(object sender, EventArgs e)
+                {
+                    Random rand = new Random();
+                    int r = rand.Next(0, 300);
+                    this.button1.Location = new Point(r, r);
+                }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+        
+    }
+}
