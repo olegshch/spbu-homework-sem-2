9f0b5153fb0613ee760d108e76d79ef355799438
olegshch <43221802+olegshch@users.noreply.github.com>
2019-05-09 23:17:32 +0300

    заполнение по спирали

---
+++ spiral/spiral/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace spiral
+{
+    class Program
+    {
+        static void Move (int i, int j, int edge, int val,  int [,] arr)
+        {
+            if (i >= 0 && j >= 0)
+            {
+                for (int k = 0; k < edge - 1; k++)
+                {
+                    arr[i + k, j] = val;
+                    val++;
+                }
+                for (int k = 0; k < edge - 1; k++)
+                {
+                    arr[i + edge-1, j + k] = val;
+                    val++;
+                }
+                for (int k = 0; k < edge - 1; k++)
+                {
+                    arr[i +edge-1- k, j+edge-1] = val;
+                    val++;
+                }
+                for (int k = 0; k < edge - 1; k++)
+                {
+                    arr[i, j+edge-1 - k] = val;
+                    val++;
+                }
+                Move(i - 1, j - 1, edge + 2, (edge - 1) * 4+2, arr);
+            }
+
+        }
+        static void Print(int [,] arr, int n)
+        {
+            for(int i = 0; i < n; i++)
+            {
+                for(int j = 0; j < n; j++)
+                {
+                    Console.Write("{0} ",arr[i, j]);
+                }
+                Console.Write("\n");
+            }
+        }
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int [,] arr = new int[n,n];
+            arr[n / 2, n / 2] = 1;
+            Move(n / 2-1, n / 2-1, 3, 2, arr);
+            Print(arr, n);
+            Console.ReadKey();
+        }
+    }
+}
