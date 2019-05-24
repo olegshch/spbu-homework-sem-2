03a2b441e56ebfe6223127adc22b300f26886a6b
olegshch <st069439@student.spbu.ru>
2019-05-24 15:16:19 +0300

    Попытка улучшения
    добавлена задержка для кнопки для одновременного "заполнение-появление"

--- ProgressBar/ProgressBar/Form1.cs
+++ ProgressBar/ProgressBar/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace ProgressBar
 {
@@ -16,8 +17,9 @@ namespace ProgressBar
         {
             InitializeComponent();
             Exit.Visible = false;
+            
         }
-
+        int count = 0;
         private void Start_Click(object sender, EventArgs e)
         {
             Time.Enabled = true;
@@ -26,11 +28,12 @@ namespace ProgressBar
         private void timer1_Tick(object sender, EventArgs e)
         {
             progressBar1.Increment(+1);
-            if (progressBar1.Value == 100)
-            {
+            count++;
+            if (progressBar1.Value == 100 && count>106)
+            {                
                 Time.Enabled = false;
                 Exit.Visible = true;
-            }           
+            }  
         }
         private void Exit_Click(object sender, EventArgs e)
         {
