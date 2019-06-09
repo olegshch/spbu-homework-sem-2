cb38beaaf55b9fe62ef8d3abc10761d971ee784f
olegshch <st069439@student.spbu.ru>
2019-06-09 17:41:24 +0300

    вроде тоже ничего лишнего
    попытался AppVeyor подключить

---
+++ Map_Filter_Fold/Map_Filter_Fold/Class1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Map_Filter_Fold
+{
+    public class Function
+    {
+        /// <summary>
+        /// Меняет каждый элемент списка (как аргумент функции) на соответствующее значение
+        /// </summary>
+        /// <param name="inlist"> Входной список</param>
+        /// <param name="function"> Функция, меняющая элементы</param>
+        public static List<int> Map(List<int> inlist, Func<int, int> function)
+        {
+            var outlist = new List<int>();
+            foreach(int element in inlist)
+            {
+                outlist.Add(function(element));
+            }
+            return outlist;
+        }
+
+        /// <summary>
+        /// Принимает список элементов и возвращает список из элементов, которые прошли через фильтр, заданный функцией
+        /// </summary>
+        /// <param name="inlist">Входной список</param>
+        /// <param name="function"> Булева функция, являющая собой фильтр</param>
+        public static List<int> Filter(List<int> inlist, Func<int, bool> function)
+        {
+            var outlist = new List<int>();
+            foreach (int element in inlist)
+            {
+                if (function(element))
+                {
+                    outlist.Add(element);
+                }
+                
+            }
+            return outlist;
+        }
+
+        /// <summary>
+        /// Меняет начальное значение функции, последовательно применяя к нему заданную функцию с каждым из элементов списка
+        /// </summary>
+        /// <param name="inlist">Входной список</param>
+        /// <param name="start">Начальное значение функции</param>
+        /// <param name="function">функция обоновления результата</param>
+        /// <returns></returns>
+        public static int Fold(List<int> inlist, int start, Func<int, int, int> function)
+        {
+            var final = start;
+            foreach (int element in inlist)
+            {
+                final = function(final, element);
+            }
+            return final;
+        }
+    }
+}
---
+++ Map_Filter_Fold/Map_Filter_FoldTests/FunctionTests.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Map_Filter_Fold;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Map_Filter_Fold.Tests
+{
+    [TestClass()]
+    public class FunctionTests
+    {
+        private List<int> list;
+        private List<int> resultlist;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            list = new List<int>();
+            resultlist = new List<int>();
+        }
+
+        [TestMethod()]
+        public void MapTest()
+        {
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            resultlist = Function.Map(list, x => x * 2);
+            for (int i = 0; i < resultlist.Count; i++)
+            {
+                Assert.AreEqual(list[i] * 2 , resultlist[i]);
+            }
+        }
+
+        [TestMethod()]
+        public void FilterTest()
+        {
+            for(int i = 0; i < 100; i++)
+            {
+                list.Add(i);
+            }
+            resultlist = Function.Filter(list, x => x % 3 == 1);
+            for (int i = 0; i < resultlist.Count; i++)
+            {
+                Assert.IsTrue(resultlist[i] % 3 == 1);
+            }
+        }
+
+        [TestMethod()]
+        public void FoldTest()
+        {
+            int known = 1;
+            for (int i = 1; i <= 10; i++)
+            {
+                list.Add(i);
+                known *= i;
+            }
+            int result = Function.Fold(list, 1, (res, curr) => res * curr);
+            Assert.AreEqual(result, known);
+              
+        }
+    }
+}
