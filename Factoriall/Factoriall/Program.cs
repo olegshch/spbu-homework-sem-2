3b5993f2c0b8babb4ab7972b73bdd7161e9a17ca
olegshch <st069439@student.spbu.ru>
2019-06-10 15:59:02 +0300

    Работает

---
+++ Factoriall/Factoriall/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factoriall
+{
+    class Program
+    {
+        public static int Fact(int number)
+        {
+            if (number <= 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return number * Fact(number - 1);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine(Fact(number));
+            
+        }
+    }
+}
