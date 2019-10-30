e63f9b38d4dc7e8d5ad937c218b85ed15d82f3cf
olegshch <43221802+olegshch@users.noreply.github.com>
2019-10-30 22:57:06 +0300

    Update Program.cs

--- TreeCalcWindow/TreeCalcWindow/Program.cs
+++ TreeCalcWindow/TreeCalcWindow/Program.cs
@@ -12,14 +12,6 @@ namespace TreeCalcWindow
         static void Main(string[] args)
         {
             TreeCalculator.TreeCalculator calc = new TreeCalculator.TreeCalculator();
-            Console.WriteLine(calc.Calculate("+ 1 * / 8 4 3"));
-            //Console.ReadKey();
-            calc.Print("+ 1 * / 8 4 3");
-            Console.WriteLine();
-            Console.WriteLine(calc.Index);
-            Console.ReadKey();
-            //calc.Calculate("+ 1 * / 8 4 3");
-            //calc.Print("+ 1 * / 8 4 3");
         }
     }
 }
