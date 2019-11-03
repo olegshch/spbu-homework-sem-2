be51fb7341f6061343dcf5a892740a3a3efa8e92
olegshch <43221802+olegshch@users.noreply.github.com>
2019-11-03 03:02:38 +0300

    Design

---
+++ GUICalc/GUI_calc/Calculator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI_calc
+{
+    public partial class Calculator : Form
+    {
+        public Calculator()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            if(!(textBox.Text == "" && ((Button)sender).Text == "0"))
+            {
+                textBox.Text += ((Button)sender).Text;
+            }
+        }
+
+        private void buttonSign_Click(object sender, EventArgs e)
+        {
+            textBox.Text = string.Concat(-1 * double.Parse(textBox.Text));
+        }
+
+        private void buttonPoint_Click(object sender, EventArgs e)
+        {
+            textBox.Text += ".";
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            textBox.Text = "";
+        }
+
+        private void buttonErase_Click(object sender, EventArgs e)
+        {
+            string expression = "";
+            for(int i = 0; i < textBox.Text.Length - 1; i++)
+            {
+                expression += textBox.Text[i];
+            }
+            textBox.Text = expression;
+        }
+    }
+}
