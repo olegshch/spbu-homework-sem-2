a451afa49f7ea8e911673eeee37215f3d15fc887
olegshch <st069439@student.spbu.ru>
2019-11-06 02:35:03 +0300

    Update Set.cs

--- GenericSet/Set.cs
+++ GenericSet/Set.cs
@@ -9,11 +9,39 @@ namespace GenericSet
             public T Data { get; set; }
             public Node Left { get; set; }
             public Node Right { get; set; }
-            public Node Parent { get; set; }
+            //public Node Parent { get; set; }
         }
 
         private Node root;
+
         public int Count { get; private set; }
+
+        /// <summary>
+        /// true, если множество IsReadOnly
+        /// </summary>
+        public bool IsReadOnly { get; set; } = false;
+
+        /// <summary>
+        /// проверка на наличие элемента в множестве со стартовым узлом
+        /// </summary>
+        /// <param name="data">искомое значение</param>
+        /// <param name="current">стартовый узел поиска</param>
+        /// <returns>true, если элемент присутствует</returns>
+        private bool ContainsWithNode(T data, Node current)
+        {
+            if (current == null)
+            {
+                return false;
+            }
+            return (current.Data.Equals(data) || ContainsWithNode(data, current.Left) || ContainsWithNode(data, current.Right));
+        }
+
+        /// <summary>
+        /// проверка на наличие элемента в множестве
+        /// </summary>
+        /// <param name="data">искомый элемент</param>
+        /// <returns>true, если элемент присутствует</returns>
+        public bool Contains(T data) => ContainsWithNode(data, root);
 
         /// <summary>
         /// очистка множества
@@ -35,7 +63,6 @@ namespace GenericSet
             {
                 current = new Node();
                 current.Data = data;
-                current.Parent = parent;
                 Count++;
                 //return true;
             }
@@ -59,11 +86,6 @@ namespace GenericSet
         /// </summary>
         /// <param name="data">значение</param>
         public void Add(T data) => AddInNode(data, root, null);
-
-        /// <summary>
-        /// true, если множество IsReadOnly
-        /// </summary>
-        public bool IsReadOnly { get; set; } = false;
 
     }
 }
